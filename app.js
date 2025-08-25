dd369c6e64be6bdaf4c381788774fb14e1e3c048
shmuel nabul <shmuel9977@gmail.com>
2025-08-25 11:11:48 +0300

    first page

---
+++ app.js
@@ -0,0 +1,20 @@
+// import express from 'express'
+// import cors from 'cors'
+// import { config } from 'dotenv'
+// config()
+// const PORT = process.env.PORT 
+
+// const app = express()
+
+// app.use(cors())
+// app.get('/',(req,res) =>{
+//     res.send('hello from server');
+// })
+
+
+
+
+// app.listen(PORT,()=>{
+//     console.log(`server run on http://localhost:${PORT}`);
+    
+// })
---
+++ component/component.js
@@ -0,0 +1,50 @@
+import { getData } from "../js/dal.js"
+
+
+function creatNewsCard(titleText, editorName, imgUrl ,linkUrl) {
+
+    const main = document.getElementById('root')
+
+    const title = document.createElement('a');
+    title.textContent = titleText;
+    title.href = linkUrl
+
+    
+    const editor = document.createElement('h4');
+    editor.textContent = editorName;
+    
+    const image = document.createElement('img');
+    image.src = imgUrl;
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.append(title, image, editor);
+    main.appendChild(card)
+
+}
+
+
+async function loadData() {
+    const articles = await getData()
+    articles?.forEach(article => {
+        creatNewsCard(article.title, article.author, article.urlToImage);
+        // console.log('article', articles);
+        
+    });
+}
+await loadData()
+
+
+
+
+
+// const root = document.getElementById('root');
+// const card1 = creatNewsCard("hello1","https://www.prog.co.il/attachments/1-jpg.715018/", "free text1", "shmuel1" );
+// const card2 = creatNewsCard("hello2","https://www.prog.co.il/attachments/1-jpg.715018/", "free text2", "shmuel2" );
+// const card3 = creatNewsCard("hello3", "free text3", "shmuel3", "https://www.prog.co.il/attachments/1-jpg.715018/");
+// const card4 = creatNewsCard("hello4", "free text4", "shmuel4", "https://www.prog.co.il/attachments/1-jpg.715018/");
+// const card5 = creatNewsCard("hello4", "free text4", "shmuel4", "https://www.prog.co.il/attachments/1-jpg.715018/");
+// const card6 = creatNewsCard("hello4", "free text4", "shmuel4", "https://www.prog.co.il/attachments/1-jpg.715018/");
+// const card7 = creatNewsCard("hello4", "free text4", "shmuel4", "https://www.prog.co.il/attachments/1-jpg.715018/");
+// const card8 = creatNewsCard("hello4", "free text4", "shmuel4", "https://www.prog.co.il/attachments/1-jpg.715018/");
+// root.append(card1, card2, card3, card4,card5,card6,card7,card8);
