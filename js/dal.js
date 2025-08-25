207c6d11a6a27806ac42d7108ff456b30bbd0093
shmuel nabul <shmuel9977@gmail.com>
2025-08-25 12:08:17 +0300

    bug fixed

--- js/dal.js
+++ js/dal.js
@@ -1,18 +1,19 @@
 
 export async function getData() {
     const storage = localStorage.getItem("news")
-    console.log(storage);
+    // console.log(storage);
 
     if (storage) {
         return JSON.parse(storage)
     }
     else {
         const apiKey = window.config.key;
-        const data = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2025-07-25&sortBy=publishedAt&apiKey=${apiKey}`);
+        const data = await fetch(`https://newsapi.org/v2/everything?q=news&sortBy=publishedAt&apiKey=${apiKey}`);
 // console.log('api',apiKey);
 
         const result = await data.json()
-        if (result.articles)
+        if (result)
+            
             localStorage.setItem("news", JSON.stringify(result.articles))
         return result.articles
 
